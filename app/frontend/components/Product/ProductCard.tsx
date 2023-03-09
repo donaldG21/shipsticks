d1b4bfdd73ae75b1465d3752f4b9cd7a433c832e
Donald Granados <donald.g@me.com>
2023-03-08 22:35:13 -0500

    Extract ProductCard

---
+++ app/frontend/components/Product/ProductCard.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react'
+import Product from '../../types/product';
+
+interface ProductCardProps {
+  product: Product;
+}
+
+export const ProductCard: FC<ProductCardProps> = ({ product }) => {
+  return (
+    <div className="w-3/5 shadow-card border-gray-300 border flex justify-center rounded-2xl bg-white">
+      <h2 className="p-8 px-3 text-xl font-semibold">
+        Use our "{`${product.type} - ${product.name}`}" to ship!
+      </h2>
+    </div>
+  )
+}
